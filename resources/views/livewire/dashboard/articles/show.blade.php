e7c43a6635eb1064fa00bcacee9fab979e652f63
Pablo Eliezer <pabloeliezer@hotmail.com>
2025-04-05 20:54:02 -0300

    Update articles show colors for dark theme

--- resources/views/livewire/dashboard/articles/show.blade.php
+++ resources/views/livewire/dashboard/articles/show.blade.php
@@ -2,13 +2,13 @@
 @section('description', $article->description)
 
 <div>
-    <a href="{{ route('home') }}" class="rounded-full bg-gray-100 flex items-center justify-center w-12 h-12">
-        <x-icons.arrow-left class="w-8 h-8" />
+    <a href="{{ route('home') }}" class="rounded-full flex items-center justify-center w-12 h-12 dark:text-gray-400 dark:hover:text-white">
+        <x-icons.arrow-left class="w-8 h-8 " />
     </a>
-    <p class="border-l-4 border-gray-300 pl-3 mt-8">
+    <p class="border-l-4 border-gray-300 pl-3 mt-8 dark:text-gray-400">
         {{ $article->published_at?->format('M d, Y') }}
     </p>
-    <h1 class="text-5xl font-bold mt-8">
+    <h1 class="text-5xl font-bold mt-8 dark:text-white">
         {{ $article->title }}
     </h1>
     <div class="py-4 flex justify-center">
@@ -17,7 +17,7 @@
              class="max-w-full h-96 object-cover rounded-lg"
          />
     </div>
-    <div class="mt-10 flex flex-col space-y-2" id="article-body">
+    <div class="mt-10 flex flex-col space-y-2 dark:text-white" id="article-body">
         {!! $article->body !!}
     </div>
 </div>
